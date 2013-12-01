9c5cc05032af237e24f73e75ac68c130c53b2b52
Jun Yang <yangjvn@126.com>
2013-12-01 13:11:45 +0800

    rename to marktexedt

---
+++ MarkTexEdt/MarkTexEdt/util/ObservableClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkTexEdt.util
+{
+    /// <summary>
+    /// 实现NotifyPropertyChanged 接口，属性的改动将自动更新界面
+    /// </summary>
+    [Serializable]
+    public class ObservableClass : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// 系统会自动为该事件生成处理函数
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ MarkTexEdt/MarkTexEdt/util/VisibilityToBoolConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MarkTexEdt.util
+{
+    /// <summary>
+    /// 方便前台绑定后台数据
+    /// </summary>
+    [ValueConversion(typeof(Visibility), typeof(bool))]
+    public class VisibilityToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((Visibility)value == Visibility.Visible) return true;
+            else return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+        }
+    }
+}
