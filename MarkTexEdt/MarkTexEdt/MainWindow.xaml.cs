c33249eed93db723b219496e4e6ccf2711a132a7
dusx5 <dushixian5@126.com>
2013-12-09 15:51:51 +0800

    Implementation some buttons funtion

--- MarkTexEdt/MarkTexEdt/MainWindow.xaml.cs
+++ MarkTexEdt/MarkTexEdt/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
+//using System.Windows.Forms
 
 namespace MarkTexEdt
 {
@@ -60,10 +61,10 @@ namespace MarkTexEdt
         /// <param name="e"></param>
         private void tbEditor_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Uri uri = new Uri(@"http://www.baidu.com", UriKind.Absolute);
+           // Uri uri = new Uri(@"http://www.baidu.com", UriKind.Absolute);
 
             //预览HTML页面
-            wbViewer.Navigate(uri);
+          //  wbViewer.Navigate(uri);
 
         }
 
@@ -143,7 +144,10 @@ namespace MarkTexEdt
         /// <param name="e"></param>
         private void Undo_Click(object sender, RoutedEventArgs e)
         {
-            
+            if (tbEditor.CanUndo == true)
+            {
+                tbEditor.Undo();
+            }
         }
 
         /// <summary>
@@ -155,6 +159,158 @@ namespace MarkTexEdt
         {
             config.SaveWindow(this);
             config.SaveToFile();
+        }
+        /// <summary>
+        /// 对选择的文本进行加粗，若是没选择，则对接下去输入的字符加粗
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Bold_Click(object sender, RoutedEventArgs e)
+        {
+            TextSelection selection = tbEditor.Selection;
+            // 如果没有选择文本，则按选取了一个普通字体的文本来处理
+            FontWeight currentState = FontWeights.Normal;
+            // 尝试获取所选文本的粗体状态
+            if (selection.GetPropertyValue(Run.FontWeightProperty) !=
+              DependencyProperty.UnsetValue)
+            {
+                currentState = (FontWeight)selection.GetPropertyValue(Run.FontWeightProperty);
+            }
+            if (currentState == FontWeights.Normal)
+            {
+                selection.ApplyPropertyValue(Run.FontWeightProperty, FontWeights.Bold);
+            }
+            else
+            {
+                selection.ApplyPropertyValue(Run.FontWeightProperty, FontWeights.Normal);
+            }
+
+            TextRange a = new TextRange(tbEditor.Document.ContentStart, tbEditor.Document.ContentEnd);           
+            String strText = a.Text.ToString();           
+            TextPointer tpEnd = tbEditor.Document.ContentStart.GetPositionAtOffset(strText.Length);
+            TextRange b = new TextRange(tbEditor.Document.ContentStart, tpEnd);
+            String strDes = "**" + b.Text + "**";
+            Console.WriteLine(strDes);   
+                   
+            // 这个细节很关键，实现将焦点返回给文本框，这样用户将可以直接再更改设置的字体
+            tbEditor.Focus();
+        }
+        /// <summary>
+        /// 对选择的文本进行斜体，若是没选择，则接下去输入的字符会是斜体
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Italic_Click(object sender, RoutedEventArgs e)
+        {
+            TextSelection selection = tbEditor.Selection;
+            // 如果没有选择文本，则按选取了一个普通字体的文本来处理
+            //FontWeight currentState = FontWeights.Normal;
+            FontStyle currentState = FontStyles.Normal;
+            // 尝试获取所选文本的粗体状态
+           if (selection.GetPropertyValue(Run.FontStyleProperty) !=
+              DependencyProperty.UnsetValue)
+            {
+                  currentState = (FontStyle)selection.GetPropertyValue(Run.FontStyleProperty);  
+                //currentState = (FontWeight)selection.GetPropertyValue(Run.FontWeightProperty);
+            }
+            if (currentState == FontStyles.Normal)
+            {
+                selection.ApplyPropertyValue(Run.FontStyleProperty, FontStyles.Italic);
+            }
+            else
+            {
+                selection.ApplyPropertyValue(Run.FontStyleProperty, FontStyles.Normal);
+               // selection.ApplyPropertyValue(Run.FontWeightProperty, FontWeights.Normal);
+            }
+
+            TextRange a = new TextRange(tbEditor.Document.ContentStart, tbEditor.Document.ContentEnd);
+            String strText = a.Text.ToString();
+            TextPointer tpEnd = tbEditor.Document.ContentStart.GetPositionAtOffset(strText.Length);
+            TextRange b = new TextRange(tbEditor.Document.ContentStart, tpEnd);
+            String strDes = "*" + b.Text + "*";
+            Console.WriteLine(strDes);   
+
+            // 这个细节很关键，实现将焦点返回给文本框，这样用户将可以直接再更改设置的字体
+            tbEditor.Focus();
+        }
+
+        private void Left_Click(object sender, RoutedEventArgs e)
+        {/*
+            tbEditor.LineLeft();
+            Console.Write("left");
+            tbEditor.Focus();*/
+        }
+
+        private void Center_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Right_Click(object sender, RoutedEventArgs e)
+        {
+          //  TextSelection selection = tbEditor.Selection;
+           // tbEditor.HorizontalContentAlignment = HorizontalAlignment.Right;
+        /*    if (tbEditor.HorizontalContentAlignment == HorizontalAlignment.Right)
+            {
+                tbEditor.HorizontalContentAlignment = HorizontalAlignment.Left;
+                //  tbEditor.Content = "Control horizontal alignment changes from left to right.";
+
+            }
+            else
+            {
+                tbEditor.HorizontalContentAlignment = HorizontalAlignment.Right;
+                // tbEditor.Content = "HorizontalContentAlignment";
+            }*/
+
+          //  tbEditor.Focus();
+        }
+        /// <summary>
+        /// 重做
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Redo_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbEditor.CanRedo == true)
+            {
+                tbEditor.Redo();
+            }
+        }
+        /// <summary>
+        /// 实现全选功能
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SelectAll_Click(object sender, RoutedEventArgs e)
+        {
+            //TextSelection selection = tbEditor.Selection;
+            tbEditor.SelectAll();
+        }
+        /// <summary>
+        ///  复制
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Copy_Click(object sender, RoutedEventArgs e)
+        {
+            tbEditor.Copy();
+        }
+        /// <summary>
+        /// 剪切
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Cut_Click(object sender, RoutedEventArgs e)
+        {
+            tbEditor.Cut();
+        }
+        /// <summary>
+        /// 粘贴
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Paste_Click(object sender, RoutedEventArgs e)
+        {
+            tbEditor.Paste();
         }
 
     }
