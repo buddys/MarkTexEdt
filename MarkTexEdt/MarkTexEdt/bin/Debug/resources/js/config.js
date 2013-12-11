89eae96abda061f662dc7eb65a8f387b722d186e
Jun Yang <yangjvn@126.com>
2013-12-11 12:17:42 +0800

    resource

---
+++ MarkTexEdt/MarkTexEdt/bin/Debug/resources/js/config.js
@@ -0,0 +1,33 @@
+﻿//$(function () {
+//    var $content = $('#content');
+
+//    var markedOptions = {
+//        gfm: true,
+//        pedantic: false,
+//        sanitize: false,
+//        tables: true,
+//        smartLists: true,
+//        breaks: true
+//        /*langPrefix: 'language-',
+//        math: mathify,
+//        highlight: function (codeText, codeLanguage) { return highlightSyntax(hljs, codeText, codeLanguage);*/
+//    };
+//    //$content.html( marked($content.html(), markedOptions));
+//});
+
+window.onload = function () {
+    var markedOptions = {
+        gfm: true,
+        pedantic: false,
+        sanitize: false,
+        tables: true,
+        smartLists: true,
+        breaks: true
+        /*langPrefix: 'language-',
+        math: mathify,
+        highlight: function (codeText, codeLanguage) { return highlightSyntax(hljs, codeText, codeLanguage);*/
+    };
+    var content = document.getElementById("src");
+    var content = content.textContent;
+    document.getElementById("dst").innerHTML = marked(content, markedOptions);
+};
